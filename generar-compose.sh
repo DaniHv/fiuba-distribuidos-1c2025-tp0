85f215662ca2cc3478428b40a8ef01f37b1fa3cb
Daniel Hurtado <hurtdany@gmail.com>
2025-03-20 20:08:28 -0300

    ej1: implement generar-compose script

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,8 +1,18 @@
-name: tp0
+#!/bin/bash
+output_path=$1
+clients=$2
+
+if ! [[ $clients =~ ^[0-9]+$ && $clients -ge 1 ]] ; then
+  echo "Invalid usage: clients is expected to be an int greater than 0. Expected usage: $0 {output} {clients}." >&2; exit 1
+fi
+
+echo "Generating docker-compose file for $clients clients"
+
+output="name: tp0
 
 # Shared/Commons
 x-client-base: &client-base
-  container_name: client
+  container_name: client$i
   image: client:latest
   entrypoint: /client
   environment:
@@ -24,14 +34,20 @@ services:
     networks:
       - testing_net
 
-  client1:
-    container_name: client1
+$(for i in $(seq 1 $clients); do echo "\
+  client$i:
+    container_name: client$i
     <<: *client-base
-  
+  "
+  done
+)
 networks:
   testing_net:
     ipam:
       driver: default
       config:
         - subnet: 172.25.125.0/24
+"
 
+echo "$output" > $output_path
+echo "Docker-compose file successfully generated at $output_path"
