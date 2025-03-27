dbf661c2c39e5b728933fe2e8cd992d2d187ba76
Daniel Hurtado <hurtdany@gmail.com>
2025-03-26 23:56:10 -0300

    ej7: fix success log consulta_ganadores, replace cantidad with cant_ganadores

--- client/common/client.go
+++ client/common/client.go
@@ -211,7 +211,7 @@ func (c *Client) WaitForResults() error {
 		return err
 	}
 
-	log.Infof("action: consulta_ganadores | result: success | cantidad: %v", winnersMsg.Winners)
+	log.Infof("action: consulta_ganadores | result: success | cant_ganadores: %v", winnersMsg.Winners)
 
 	return nil
 }
